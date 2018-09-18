4e45e4f81e49e148ddbecf02f56ddd95178b8e85
ptavarez16 <ptavarez16@yahoo.com>
2018-09-17 22:16:28 -0400

    Syntax mod

--- 1.php
+++ 1.php
@@ -1,39 +1,30 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
-	<section class="content">
-
-	<aside class="col-xs-4">
-
+<section class="content">
+	
+<aside class="col-xs-4">
 	<?php Navigation();?>
-			
-			
-	</aside><!--SIDEBAR-->
-
+</aside><!--SIDEBAR-->
 
 <article class="main-content col-xs-8">
-		
-
-
-		<?php
+	<?php
 		/*
-		   Step 1:  Use the Echo Function to say hello with html h1 tags embedded
-			 inside php.
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
+		Step 1:  Use the Echo Function to say hello with html h1 tags embedded
+		inside php.
+		Step 2: Write a comment above the echo function and explain
+		what that function is doing.
 		*/
-		
-	    // This variable creates a string with a greeting to the world.
-			$greeting = 'Hello World';
-			
-      /*
-			The code below echos an h1 element with the greeting variable
-			concatenated to it.
-			*/
-			echo '<h1>'.$greeting.'</h1>';
-			
-		?>
 
-		</article><!--MAIN CONTENT-->
+		// This variable creates a string with a greeting to the world.
+		$greeting = 'Hello World';
+		
+		/*
+		The code below echos an h1 element with the greeting variable
+		concatenated to it.
+		*/
+		echo '<h1>'.$greeting.'</h1>';
+	?>
+</article><!--MAIN CONTENT-->
 
 <?php include "includes/footer.php"; ?>
