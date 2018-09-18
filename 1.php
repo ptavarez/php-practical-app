68bdf5c01f7ad2124a299fe489d0c2e286bdc467
ptavarez16 <ptavarez16@yahoo.com>
2018-09-17 22:12:40 -0400

    Add solution to first practice prompt

--- 1.php
+++ 1.php
@@ -16,21 +16,23 @@
 
 
 		<?php
-
-
 		/*
-		   Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
-
-
+		   Step 1:  Use the Echo Function to say hello with html h1 tags embedded
+			 inside php.
 		   Step 2: Write a comment above the echo function and explain
 		   what that function is doing.
-
-
-		   */
-
+		*/
+		
+	    // This variable creates a string with a greeting to the world.
+			$greeting = 'Hello World';
+			
+      /*
+			The code below echos an h1 element with the greeting variable
+			concatenated to it.
+			*/
+			echo '<h1>'.$greeting.'</h1>';
+			
 		?>
-
-	
 
 		</article><!--MAIN CONTENT-->
 
