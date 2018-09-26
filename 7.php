2bc615b033b4b3fcaf9e8ff3cf7e8c8eecd2044b
ptavarez16 <ptavarez16@yahoo.com>
2018-09-25 22:03:57 -0400

    Add create functionality

--- 7.php
+++ 7.php
@@ -9,27 +9,60 @@
 
 <article class="main-content col-xs-8">
   <?php
-    /*
-      Step 1 - Create a database in PHPmyadmin
-		  Step 2 - Create a table like the one from the lecture
-      Step 3 - Insert some Data
-    */
-    
-		// Step 4 - Connect to Database and read data
-    $connection = mysqli_connect('localhost', 'root', '', 'movies');
-    
-    if($connection) {
-      echo '<div class="col-sm-12 text-center">Popular Movies</div>';
-    } else {
-      die('Connection Fail');
+    if(isset($_POST['submit'])) {
+      global $connection;
+      $connection = mysqli_connect('localhost', 'root', '', 'movies');
+      
+      $title = $_POST['title'];
+      $genre = $_POST['genre'];
+      $rating = $_POST['rating'];
+      $release_date = $_POST['release_date'];
+      
+      if($title && $genre && $rating && $release_date) {
+        if($connection) {
+          echo '<div class="col-sm-12 text-center">Popular Movies</div>';
+        } else {
+          die('Connection Fail');
+        }
+        
+        $query = "INSERT INTO films(title, genre, rating, release_date)
+                  VALUES('$title', '$genre', '$rating', '$release_date')";
+                  
+        $result = mysqli_query($connection, $query);
+        if(!$result) {
+          die('Query FAILED' . mysqli_error());
+        }
+      } else {
+        echo "All Fields Required!";
+      }
     }
     
-    $query = "SELECT * FROM  films";
-    $result = mysqli_query($connection, $query);
+    $query2 = "SELECT * FROM  films";
+    $result2 = mysqli_query($connection, $query2);
 	?>
-  <div class="col-sm-6">
+  <form class="" action="7.php" method="post">
+    <div class="form-group">
+      <label for="title">Title</label>
+      <input type="text" name="title" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="genre">Genre</label>
+      <input type="text" name="genre" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="rating">Rating</label>
+      <input type="text" name="rating" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="release_date">Release Date</label>
+      <input type="text" name="release_date" class="form-control">
+    </div>
+    <input class="btn btn-primary" type="submit" name="submit" value="Create">
+  </form>
+  <br>
+  <div class="col-sm-12">
     <?php
-      while($row = mysqli_fetch_assoc($result)) {
+      while($row = mysqli_fetch_assoc($result2)) {
     ?>
     <pre>
       <?php print_r($row); ?>
