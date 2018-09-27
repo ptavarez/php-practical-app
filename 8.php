afb31ff863d6f4fe4af81d7e56fe433ccdd20379
ptavarez16 <ptavarez16@yahoo.com>
2018-09-26 22:05:19 -0400

    Add solution

--- 8.php
+++ 8.php
@@ -1,37 +1,27 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
+<aside class="col-xs-4">
+	<?php Navigation();?>
+</aside><!--SIDEBAR-->
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
-
-
+<article class="main-content col-xs-8">
+	<?php
+		// Step 1 - Make a variable with some text as value
+		$sentence = 'I\'m a variable in disguise';
+		$salt = 'iusesomecrazystrings22';
+		$hashFormat = '$2y$';
+		$hashSalt = $hashFormat.$salt;
 		
-	<article class="main-content col-xs-8">
-	
-	
-	<?php  
-
-	/*  Step 1 -Make a variable with some text as value
-
-		Step 2 - Use crypt() function to encrypt it
-
-		Step 3 - Assign the crypt result to a variable
-
-		Step 4 - echo the variable
-
-	*/
-	
+		// Step 2 - Use crypt() function to encrypt it
+		// Step 3 - Assign the crypt result to a variable
+		$cryp = crypt($sentence, $hashSalt);
+		
+		// Step 4 - echo the variable
+		echo $cryp;
 	?>
-
-
-
-
-
 </article><!--MAIN CONTENT-->
+
 <?php include "includes/footer.php"; ?>
