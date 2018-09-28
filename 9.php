9a37ab10312f5b2deb0fda6df58f628d34bb9291
ptavarez16 <ptavarez16@yahoo.com>
2018-09-27 21:11:27 -0400

    Add Solution

--- 9.php
+++ 9.php
@@ -1,37 +1,33 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
 
+<section class="content">
 
+<aside class="col-xs-4">
+	<?php Navigation();?>
+</aside><!--SIDEBAR-->
 
-	<section class="content">
-
-		<aside class="col-xs-4">
-
-		<?php Navigation();?>
+<article class="main-content col-xs-8">
+	<?php
+			// Step 1 - Create a link saying Click Here, and set the link href to pass
+			// some parameters and use the GET super global to see it.
+			$rando = rand(100, 2000);
+			print_r($_GET);
 			
-			
-		</aside><!--SIDEBAR-->
+			// Step 2 - Set a cookie that expires in one week.
+			$name = 'CookieMonstah';
+			$value = 1994;
+			$exp = time() + (60*60*24*7);
+			setcookie($name, $value, $exp);
 
-
-			<article class="main-content col-xs-8">
-			
-		
-	
-	<?php 
-
-	/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
-
-		Step 2 - Set a cookie that expires in one week
-
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
-	
+			// Step 3 - Start a session and set it to value, any value you want.
+			session_start();
+			$_SESSION['$bornYear'] = $_COOKIE['CookieMonstah'];
+			echo "<br>";
+			echo "Dro was bron on ".$_SESSION['$bornYear'];
 	?>
-
-
-
-
-
+	<br>
+	<a href="9.php?id=<?php echo $rando?>">Click Here</a>
 </article><!--MAIN CONTENT-->
+
 <?php include "includes/footer.php"; ?>
