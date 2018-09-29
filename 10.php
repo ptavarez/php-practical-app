6de01297cc354e95939dfcb4b556601af6e4a47c
ptavarez16 <ptavarez16@yahoo.com>
2018-09-28 20:52:55 -0400

    Add solution

--- 10.php
+++ 10.php
@@ -1,40 +1,39 @@
 <?php include "functions.php"; ?>
 <?php include "includes/header.php";?>
+
 <section class="content">
 
-	<aside class="col-xs-4">
-
-		<?php Navigation();?>
-			
-			
-	</aside><!--SIDEBAR-->
+<aside class="col-xs-4">
+	<?php Navigation();?>
+</aside><!--SIDEBAR-->
 
 <article class="main-content col-xs-8">
-
-	
-	<?php  
-
-	/*  Step 1: Use the Make a class called Dog
-
-		Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
-
-		Step 4: Make a method named ShowAll that echos all the properties
-
-		Step 5: Instantiate the class / create object and call it pitbull
-
-Step 6: Call the method ShowAll
-
-	
-
-		
-	*/
-	
+	<?php
+		// Step 1: Make a class called Dog
+		// Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
+		// Step 3: Make a method named ShowAll that echos all the properties
+			class Dog {
+				var $breed = 'Pitbull';
+				var $eyeColor = 'Sky Blue';
+				var $furColor = 'White';
+				var $age = 5;
+				
+				function ShowAll() {
+					$deats = [
+						'breed' => $this->breed,
+						'eye_color' => $this->eyeColor,
+						'fur_color' => $this->furColor,
+						'age' => $this->age
+					];
+					print_r($deats);
+				}
+			}
+		// Step 4: Instantiate the class / create object and call it pitbull
+		// Step 5: Call the method ShowAll
+			$pitbull = new Dog;
+			$pitbull->ShowAll();
+			
 	?>
-
-
-
-
-
 </article><!--MAIN CONTENT-->
 
 <?php include "includes/footer.php"; ?>
